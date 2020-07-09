a9eb0deb85b904bca862a5b7407a445441ed1fd6
Juan Manuel Medina <jmmedina@kodr.club>
2020-07-09 19:13:17 +0200

    sign in WIP

--- auth/src/routes/signin.ts
+++ auth/src/routes/signin.ts
@@ -1,9 +1,22 @@
-import express from 'express';
+import express, { Request, Response } from 'express'
+import { body } from 'express-validator'
+import { validateRequest } from '../middlewares/validate-requests'
 
-const router = express.Router();
+const router = express.Router()
 
-router.post('/api/users/signin', (req, res) => {
-  res.send('hello from in');
-});
+router.post(
+  '/api/users/signin',
+  [
+    body('email').isEmail().withMessage('Email must be valid'),
+    body('password')
+      .trim()
+      .notEmpty()
+      .withMessage('You must supply a password'),
+  ],
+  validateRequest,
+  (req: Request, res: Response) => {
+    res.send('hello from in')
+  }
+)
 
-export { router as signinRouter };
+export { router as signinRouter }
