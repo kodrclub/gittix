e2b15ae3fff1ce5b8f4aef26a9f5efd7712100aa
Juan Manuel Medina <jmmedina@kodr.club>
2020-07-20 18:33:20 +0200

    require-auth middleware

---
+++ auth/src/errors/not-authorized-error.ts
@@ -0,0 +1,17 @@
+import { CustomError } from './custom-error'
+
+export class NotAuthorizedError extends CustomError {
+  statusCode = 401
+  reason = 'Not authorized'
+
+  constructor() {
+    super('!!! NotAuthorizedError !!!')
+
+    //Only bc we're extending a built-in class
+    Object.setPrototypeOf(this, NotAuthorizedError.prototype)
+  }
+
+  serializeErrors() {
+    return [{ message: this.reason }]
+  }
+}
