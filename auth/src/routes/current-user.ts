984b40df22e3fc70dcc5a372f557e3682985454f
Juan Manuel Medina <jmmedina@kodr.club>
2020-07-20 14:02:34 +0200

    current user

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -1,9 +1,18 @@
-import express from 'express';
+import express from 'express'
+import jwt from 'jsonwebtoken'
 
-const router = express.Router();
+const router = express.Router()
 
 router.get('/api/users/current-user', (req, res) => {
-  res.send('hello from current');
-});
+  if (!req.session?.jwt) {
+    return res.send({ currentUser: null })
+  }
+  try {
+    const payload = jwt.verify(req.session.jwt, process.env.JWT_KEY!)
+    res.send({ currentUser: payload })
+  } catch (err) {
+    res.send({ currentUser: null })
+  }
+})
 
-export { router as currentUserRouter };
+export { router as currentUserRouter }
