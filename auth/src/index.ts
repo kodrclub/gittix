13699c509c8c8f7d8d0dc1621f8231c37b209f6b
Juan Manuel Medina <jmmedina@kodr.club>
2020-07-21 12:49:48 +0200

    app.ts

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,35 +1,5 @@
-import express from 'express'
-import { json } from 'body-parser'
-import 'express-async-errors'
-import cookieSession from 'cookie-session'
+import { app } from './app'
 import mongoose from 'mongoose'
-
-import { NotFoundError } from './errors/not-found-error'
-import { errorHandler } from './middlewares/error-handler'
-import { currentUserRouter } from './routes/current-user'
-import { signinRouter } from './routes/signin'
-import { signoutRouter } from './routes/signout'
-import { signupRouter } from './routes/signup'
-
-const app = express()
-app.set('trust proxy', true)
-app.use(json())
-app.use(
-  cookieSession({
-    signed: false,
-    secure: true,
-  })
-)
-app.use(currentUserRouter)
-app.use(signinRouter)
-app.use(signoutRouter)
-app.use(signupRouter)
-
-app.all('*', async () => {
-  throw new NotFoundError()
-})
-
-app.use(errorHandler)
 
 const start = async () => {
   if (!process.env.JWT_KEY) {
