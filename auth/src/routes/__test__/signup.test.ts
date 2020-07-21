21166ba44882886cacf6cbdb2c39c3e5353bc33b
Juan Manuel Medina <jmmedina@kodr.club>
2020-07-21 17:03:56 +0200

    Passing first test

---
+++ auth/src/routes/__test__/signup.test.ts
@@ -0,0 +1,11 @@
+import request from 'supertest'
+import { app } from '../../app'
+
+it('returns a 201 on successful signup', async () =>
+  request(app)
+    .post('/api/users/signup')
+    .send({
+      email: 'test@test.com',
+      password: 'password',
+    })
+    .expect(201))
