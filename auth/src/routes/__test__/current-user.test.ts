dff4c10de86bc572ab8d3a47c3937bbf40065a82
Juan Manuel Medina <jmmedina@kodr.club>
2020-07-21 19:30:46 +0200

    global.authenticate helper

--- auth/src/routes/__test__/current-user.test.ts
+++ auth/src/routes/__test__/current-user.test.ts
@@ -2,14 +2,7 @@ import request from 'supertest'
 import { app } from '../../app'
 
 it('responds with details about the current user', async () => {
-  const authResponse = await request(app)
-    .post('/api/users/signup')
-    .send({
-      email: 'test@test.com',
-      password: 'password',
-    })
-    .expect(201)
-  const cookie = authResponse.get('Set-Cookie')
+  const cookie = await global.authenticate()
 
   const response = await request(app)
     .get('/api/users/current-user')
@@ -19,4 +12,14 @@ it('responds with details about the current user', async () => {
 
   // console.log(response.body)
   expect(response.body.currentUser.email).toEqual('test@test.com')
+})
+
+it('responds with undefined if not signed in', async () => {
+  const response = await request(app)
+    .get('/api/users/current-user')
+    .send()
+    .expect(200)
+
+  // console.log(response.body)
+  expect(response.body.currentUser).toBeUndefined()
 })
