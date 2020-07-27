f2fc6644a81387c64a3c01a98aa38f20e7ddd29b
Juan Manuel Medina <jmmedina@kodr.club>
2020-07-27 17:22:40 +0200

    Tickets service stub ok

--- tickets/src/app.ts
+++ tickets/src/app.ts
@@ -4,10 +4,6 @@ import 'express-async-errors'
 import cookieSession from 'cookie-session'
 
 import { errorHandler, NotFoundError } from '@kodrclub-tickets/common'
-import { currentUserRouter } from './routes/current-user'
-import { signinRouter } from './routes/signin'
-import { signoutRouter } from './routes/signout'
-import { signupRouter } from './routes/signup'
 
 const app = express()
 app.set('trust proxy', true)
@@ -18,10 +14,6 @@ app.use(
     secure: process.env.NODE_ENV != 'test',
   })
 )
-app.use(currentUserRouter)
-app.use(signinRouter)
-app.use(signoutRouter)
-app.use(signupRouter)
 
 app.all('*', async () => {
   throw new NotFoundError()
