2311be28c583e31340fecdfeaf5d1d5b72436b22
Juan Manuel Medina <jmmedina@kodr.club>
2020-07-30 16:41:03 +0200

    Common build 1.0.18

--- tickets/src/routes/update.ts
+++ tickets/src/routes/update.ts
@@ -1,5 +1,6 @@
 import express, { Request, Response } from 'express'
 import { body } from 'express-validator'
+import { natsWrapper } from '../nats-wrapper'
 import {
   NotFoundError,
   NotAuthorizedError,
@@ -7,6 +8,7 @@ import {
   validateRequest,
 } from '@kodrclub-tickets/common'
 import { Ticket } from '../models/ticket'
+import { TicketUpdatedPublisher } from '../events/publishers/ticket-updated-publisher'
 
 const router = express.Router()
 
@@ -35,6 +37,12 @@ router.put(
       price: req.body.price,
     })
     await ticket.save()
+    await new TicketUpdatedPublisher(natsWrapper.client).publish({
+      id: ticket.id,
+      title: ticket.title,
+      price: ticket.price,
+      userId: ticket.userId,
+    })
 
     res.send(ticket)
   }
