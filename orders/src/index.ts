36f51f7c9a22c0c7605b2aeccaf70074f425fe90
Juan Manuel Medina <jmmedina@kodr.club>
2020-07-31 13:20:43 +0200

    Orders microservice stub

---
+++ orders/src/index.ts
@@ -0,0 +1,47 @@
+import mongoose from 'mongoose'
+import { app } from './app'
+import { natsWrapper } from './nats-wrapper'
+
+const start = async () => {
+  if (!process.env.JWT_KEY) {
+    throw new Error('JWT_KEY not defined')
+  }
+  if (!process.env.MONGO_URI) {
+    throw new Error('MONGO_URI not defined')
+  }
+
+  if (!process.env.NATS_CLIENT_ID) {
+    throw new Error('NATS_CLIENT_ID not defined')
+  }
+  if (!process.env.NATS_CLUSTER_ID) {
+    throw new Error('NATS_CLUSTER_ID not defined')
+  }
+  if (!process.env.NATS_URL) {
+    throw new Error('NATS_URL not defined')
+  }
+
+  try {
+    await natsWrapper.connect('ticketing', 'uyiuqityi', 'http://nats-srv:4222')
+    natsWrapper.client.on('close', () => {
+      console.log('>>> tickets NATS connection closed!')
+      process.exit()
+    })
+    process.on('SIGINT', () => natsWrapper.client.close())
+    process.on('SIGTERM', () => natsWrapper.client.close())
+
+    await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+    })
+    console.log('>>> tickets connected to MongoDB')
+  } catch (err) {
+    console.log(err)
+  }
+
+  app.listen(3000, () => {
+    console.log('>>> tickets listening on port 3000')
+  })
+}
+
+start()
