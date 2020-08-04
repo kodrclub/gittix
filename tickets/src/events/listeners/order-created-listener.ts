4d7412d2607816cf15053850001142efa7eeb273
Juan Manuel Medina <jmmedina@kodr.club>
2020-08-04 14:20:06 +0200

    Change Stan client to protected

--- tickets/src/events/listeners/order-created-listener.ts
+++ tickets/src/events/listeners/order-created-listener.ts
@@ -3,6 +3,7 @@ import { Subjects, Listener, OrderCreatedEvent } from '@kc-gittix/common'
 // import { Order } from '../../models/order'
 import { Ticket } from '../../models/ticket'
 import { queueGroupName } from './queue-group-name'
+import { TicketUpdatedPublisher } from '../publishers/ticket-updated-publisher'
 
 export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
   readonly subject = Subjects.OrderCreated
