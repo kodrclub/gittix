d81132b99ad040ffeee9ef938435ab47960f18e2
Juan Manuel Medina <64071176+kodrclub@users.noreply.github.com>
2020-08-08 00:38:24 +0200

    Update index.js

--- t/index.js
+++ t/index.js
@@ -1,6 +1,8 @@
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0'
 const axios = require('axios')
 
+console.log("1")
+
 const cookie =
   'express:sess=eyJqd3QiOiJleUpoYkdjaU9pSklVekkxTmlJc0luUjVjQ0k2SWtwWFZDSjkuZXlKcFpDSTZJalZsT0dZME5qQTFOak5oWVRkbE1EQXlZMkl3TUdSbU5pSXNJbVZ0WVdsc0lqb2lkR1Z6ZERFd1FIUmxjM1F1WTI5dElpd2lhV0YwSWpveE5UZzJOVE0yTlRBMWZRLlh3R3p5UVZHYnFSaHZ1YWJDMTdsYzNtYlpQNU1XMnl1UU1kODU1Y0hEM3MifQ=='
 
