95c137d6c3723d543b47f45021964b99e0011d73
Juan Manuel Medina <jmmedina@kodr.club>
2020-08-07 17:17:54 +0200

    Orders startup message

--- orders/src/index.ts
+++ orders/src/index.ts
@@ -7,6 +7,8 @@ import { TicketCreatedListener } from './events/listeners/ticket-created-listene
 import { TicketUpdatedListener } from './events/listeners/ticket-updated-listener'
 
 const start = async () => {
+  console.log('Starting up....')
+
   if (!process.env.JWT_KEY) {
     throw new Error('JWT_KEY not defined')
   }
